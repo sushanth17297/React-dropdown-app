f3bd60bdcaf3a93defab1d42a690b82644fe8167
N Sushanth Reddy <134855114+sushanth17297@users.noreply.github.com>
2023-11-15 09:12:48 +0530

    deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/React-dropdown-app",
   server: {
     port: 8001,
   },
