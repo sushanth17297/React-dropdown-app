d2f83146345d775bd817b4f1187631677d9a7fff
N Sushanth Reddy <134855114+sushanth17297@users.noreply.github.com>
2023-11-15 09:05:11 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Selector from "./components/Selector/Selector";
+
+const App = () => {
+  return (
+    <div className="bg-gray-900 font-P3oppins flex  h-screen items-center justify-center">
+      <Selector />
+    </div>
+  );
+};
+
+export default App;
